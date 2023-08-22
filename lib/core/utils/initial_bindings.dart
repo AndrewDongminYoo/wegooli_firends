a82a541fe3c70c79036582ad7a25b1da94714a80
Dongmin Yu <ydm2790@gmail.com>
2023-08-22 13:04:00 +0900

    revert: ⏪️ api 클라이언트 생성코드 재생성 위해 삭제

--- lib/core/utils/initial_bindings.dart
+++ lib/core/utils/initial_bindings.dart
@@ -1,12 +1,10 @@
 // 🌎 Project imports:
 import '/core/app_export.dart';
-import '/data/api.dart';
 
 class InitialBindings extends Bindings {
   @override
   void dependencies() {
     Get.put(PrefUtils());
-    Get.put(ApiClient());
     Connectivity connectivity = Connectivity();
     Get.put(NetworkInfo(connectivity));
   }
--- lib/data/data.dart
+++
@@ -1,2 +0,0 @@
-export 'api.dart';
-export 'models/models.dart';

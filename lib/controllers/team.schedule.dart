082ff0cbcb3e47acfd6e36cf8294e448883c6df5
Dongmin Yu <ydm2790@gmail.com>
2023-08-31 15:23:57 +0900

    refactor: 🏗️ 불필요한 재귀적 폴더 구조 제거
    so many breaking changes!

--- lib/controllers/team.schedule.dart
+++ lib/controllers/team.schedule.dart
@@ -12,7 +12,7 @@ class ScheduleController extends GetxController {
 
   SelectionPopupModel? selectedDropDownValue;
   Rx<List<SelectionPopupModel>> dropdownItemList = Rx([
-    SelectionPopupModel(id: 1, title: "Item One", isSelected: true),
+    SelectionPopupModel(id: 1, title: "Item One"),
     SelectionPopupModel(id: 2, title: "Item Two"),
     SelectionPopupModel(id: 3, title: "Item Three")
   ]);

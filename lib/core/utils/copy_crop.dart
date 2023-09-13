87aea6f996dcb719fcde35431ef1c3153ec5c21a
Dongmin Yu <ydm2790@gmail.com>
2023-09-13 21:17:45 +0900

    build: ➖ firebase auth plugin (구글,애플로그인) 제거

---
+++ lib/core/utils/copy_crop.dart
@@ -0,0 +1,173 @@
+// 📦 Package imports:
+import 'package:image/image.dart' show Image, Pixel;
+
+/// Returns a cropped copy of [src].
+Image copyCrop(Image src,
+    {required int x,
+    required int y,
+    required int width,
+    required int height,
+    num radius = 0,
+    bool antialias = true}) {
+  // Make sure crop rectangle is within the range of the src image.
+  x = x.clamp(0, src.width - 1).toInt();
+  y = y.clamp(0, src.height - 1).toInt();
+  if (x + width > src.width) {
+    width = src.width - x;
+  }
+  if (y + height > src.height) {
+    height = src.height - y;
+  }
+
+  if (radius > 0 && src.hasPalette) {
+    src = src.convert(numChannels: src.numChannels);
+  }
+
+  Image? firstFrame;
+  final numFrames = src.numFrames;
+  for (var i = 0; i < numFrames; ++i) {
+    final frame = src.frames[i];
+    final dst = firstFrame?.addFrame() ??
+        Image.fromResized(frame,
+            width: width, height: height, noAnimation: true);
+    firstFrame ??= dst;
+
+    if (radius > 0) {
+      final rad = radius.round();
+      final rad2 = rad * rad;
+      final x1 = x;
+      final y1 = y;
+      final x2 = x + width;
+      final y2 = y + height;
+      final c1x = x1 + rad - 1;
+      final c1y = y1 + rad - 1;
+      final c2x = x2 - rad + 1;
+      final c2y = y1 + rad - 1;
+      final c3x = x2 - rad + 1;
+      final c3y = y2 - rad + 1;
+      final c4x = x1 + rad - 1;
+      final c4y = y2 - rad + 1;
+
+      final iter = src.getRange(x1, y1, width, height);
+      while (iter.moveNext()) {
+        final p = iter.current;
+        final px = p.x;
+        final py = p.y;
+
+        num a = 1;
+        if (px < c1x && py < c1y) {
+          a = circleTest(p, c1x, c1y, rad2, antialias: antialias);
+          if (a == 0) {
+            dst.setPixelRgba(p.x - x1, p.y - y1, 0, 0, 0, 0);
+            continue;
+          }
+        } else if (px > c2x && py < c2y) {
+          a = circleTest(p, c2x, c2y, rad2, antialias: antialias);
+          if (a == 0) {
+            dst.setPixelRgba(p.x - x1, p.y - y1, 0, 0, 0, 0);
+            continue;
+          }
+        } else if (px > c3x && py > c3y) {
+          a = circleTest(p, c3x, c3y, rad2, antialias: antialias);
+          if (a == 0) {
+            dst.setPixelRgba(p.x - x1, p.y - y1, 0, 0, 0, 0);
+            continue;
+          }
+        } else if (px < c4x && py > c4y) {
+          a = circleTest(p, c4x, c4y, rad2, antialias: antialias);
+          if (a == 0) {
+            dst.setPixelRgba(p.x - x1, p.y - y1, 0, 0, 0, 0);
+            continue;
+          }
+        }
+
+        if (a != 1) {
+          dst.getPixel(p.x - x1, p.y - y1).setRgba(p.r, p.g, p.b, p.a * a);
+        } else {
+          dst.setPixel(p.x - x1, p.y - y1, p);
+        }
+      }
+    } else {
+      for (Pixel p in dst) {
+        p.set(frame.getPixel(x + p.x, y + p.y));
+      }
+    }
+  }
+
+  return firstFrame!;
+}
+
+/// Test if the pixel [p] is within the circle centered at [x],[y] with a
+/// squared radius of [rad2]. This will test the corners, edges, and center
+/// of the pixel and return the ratio of samples within the circle.
+num circleTest(Pixel p, int x, int y, num rad2, {bool antialias = true}) {
+  /*if (!antialias) {
+    final dx1 = p.x - x;
+    final dy1 = p.y - y;
+    final d1 = dx1 * dx1 + dy1 * dy1;
+    return d1 <= rad2 ? 1 : 0;
+  }*/
+
+  var total = 0;
+  final dx1 = p.x - x;
+  final dy1 = p.y - y;
+  final d1 = dx1 * dx1 + dy1 * dy1;
+  final r1 = d1 <= rad2 ? 1 : 0;
+  total += r1;
+
+  final dx2 = (p.x + 1) - x;
+  final dy2 = p.y - y;
+  final d2 = dx2 * dx2 + dy2 * dy2;
+  final r2 = d2 <= rad2 ? 1 : 0;
+  total += r2;
+
+  final dx3 = (p.x + 1) - x;
+  final dy3 = (p.y + 1) - y;
+  final d3 = dx3 * dx3 + dy3 * dy3;
+  final r3 = d3 <= rad2 ? 1 : 0;
+  total += r3;
+
+  final dx4 = p.x - x;
+  final dy4 = (p.y + 1) - y;
+  final d4 = dx4 * dx4 + dy4 * dy4;
+  final r4 = d4 <= rad2 ? 1 : 0;
+  total += r4;
+
+  //return total / 4;
+
+  final dx5 = (p.x + 0.5) - x;
+  final dy5 = p.y - y;
+  final d5 = dx5 * dx5 + dy5 * dy5;
+  final r5 = d5 <= rad2 ? 1 : 0;
+  total += r5;
+
+  final dx6 = (p.x + 0.5) - x;
+  final dy6 = (p.y + 1) - y;
+  final d6 = dx6 * dx6 + dy6 * dy6;
+  final r6 = d6 <= rad2 ? 1 : 0;
+  total += r6;
+
+  final dx7 = p.x - x;
+  final dy7 = (p.y + 0.5) - y;
+  final d7 = dx7 * dx7 + dy7 * dy7;
+  final r7 = d7 <= rad2 ? 1 : 0;
+  total += r7;
+
+  final dx8 = (p.x + 1) - x;
+  final dy8 = (p.y + 0.5) - y;
+  final d8 = dx8 * dx8 + dy8 * dy8;
+  final r8 = d8 <= rad2 ? 1 : 0;
+  total += r8;
+
+  final dx9 = (p.x + 0.5) - x;
+  final dy9 = (p.y + 0.5) - y;
+  final d9 = dx9 * dx9 + dy9 * dy9;
+  final r9 = d9 <= rad2 ? 1 : 0;
+  total += r9;
+
+  return antialias
+      ? total / 9
+      : total > 0
+          ? 1
+          : 0;
+}
---
+++ pubdev/safe_area.md
@@ -0,0 +1,58 @@
+# Flutter Safe Area
+
+![miro.medium.com](https://miro.medium.com/v2/resize:fit:1400/1*EnnFco3P-0QkUeThpLeZFg.jpeg)
+
+[![Murtaza Sulaihi](https://miro.medium.com/v2/resize:fill:88:88/1*y9C4_JJpsekma9O8pW5faw.jpeg)](https://medium.com/@murtazasulaihi?source=post_page-----523287ff69cf--------------------------------)
+[![Flutter Community](https://miro.medium.com/v2/resize:fill:48:48/1*nE4OFcqk2kx2-Lzhey8QKA.png)](https://medium.com/flutter-community?source=post_page-----523287ff69cf--------------------------------)
+
+저는 6개월 동안 Flutter 앱을 배우고 개발해 왔습니다. 개발을 시작하면서 위젯을 이해하기 위해 여러 글과 동영상을 찾아보면서 위젯의 전체 그림을 파악하고, 위젯이 하는 일과 위젯의 모든 구성 요소를 이해하려고 노력했습니다.
+
+> 그래서 이번 글은 Flutter 위젯 SafeArea에 대해 처음으로 작성하는 글이며, 저와 같은 다른 개발자들이 웹을 검색하고, 기사를 읽고, 동영상을 보면서 여기저기서 정보를 수집하지 않아도 되도록 모든 구성 요소를 다루려고 노력할 것이며, 솔직히 시간이 많이 걸립니다. 제대로 해보고 싶어요...
+
+요즘 휴대폰에는 수백 가지의 다양한 디자인이 있으며 플러터는 모두를 위해 개발하기 위해 최선을 다하고 있습니다. 화면 크기를 늘리기 위해 휴대폰에는 아이폰 노치, 미디엄 노치, 워터드롭 노치와 같은 노치가 있으며, 개발중인 애플리케이션 SafeArea 위젯은 기본 운영 체제 프로그램의 침입을 피할 수 있도록 충분한 패딩을 제공합니다.
+
+1. **SafeArea 위젯이란 무엇인가요?**
+
+SafeArea 위젯은 노치로 인해 불필요한 클리핑과 중첩을 방지하여 기본 운영 체제의 침입을 방지하기 위해 기본 레이아웃을 감싸는 데 사용됩니다.
+
+2\. **SafeArea 위젯은 어떻게 구현하나요?
+
+```dart
+class SafeAreaWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: true,
+        bottom: true,
+        left: true,
+        right: true,
+        minimum: EdgeInsets.all(16.0),
+        maintainBottomViewPadding: true,
+        child: Text('The is the safe area widget demo'),
+      ),
+    );
+  }
+}
+```
+
+모든 위젯을 SafeArea 위젯으로 감싸면 메인 Scaffold도 SafeArea 위젯으로 감쌀 수 있으며, 특히 아이폰용 디자인 시 운영체제로 인해 상단과 하단 클리핑이 나타나지 않도록 Scaffold를 SafeArea 위젯으로 감싸는 것이 필요합니다.
+
+기본적으로 _상단, 하단, 좌측, 우측은_ true이며, _최소_ 기본 패딩은 사용자가 직접 값을 추가하여 변경할 수 있습니다. 마지막 속성인 maintainBottomViewPadding(기본값은 false)은 기본적으로 사용자가 소프트 키보드를 통해 입력이 필요한 위젯을 사용할 때 SafeArea이 아래 패딩을 유지하여 아래에 흰색 영역이 표시되고 키보드가 그 위에 위치하여 아래 패딩을 유지한다는 것을 의미합니다.
+
+3\. **팁**
+
+이 글을 쓰기 시작할 때 인터넷에 퍼져있는 정보를 조금씩 다룰 것이라고 말했듯이 위젯도 실험 해보고 다음과 같은 결과를 얻었습니다.
+
+1. Scaffold를 SafeArea로 감싸고 배경색을 사용하는 경우 전체 화면이 동일한 색상으로 덮이지 않으며, 아이폰의 경우 상단과 하단이 잘리고 안드로이드 폰의 경우 화면 상단 부분이 잘리고 기본 시스템 색상이 적용됩니다. 따라서 안전 영역 위젯을 사용하는 방법과 위치는 UI에 영향을 미치므로 반드시 확인하세요.
+2. Scaffold를 기본 위젯으로 사용하고 있고 Scaffold에 앱바를 추가한 경우 SafeArea 위젯을 사용할 필요가 없습니다. 앱바 위젯이 화면 상단을 관리합니다. 그래도 화면 하단에 있는 위젯으로 세이프 영역을 감싸고 싶을 수 있습니다.
+
+**이상입니다!
+
+> 제 글을 읽어주셔서 감사드리며, 이번 글도 첫 글이니 마음에 드셨다면 아래에 댓글을 남겨주시고 격려의 박수를 보내주세요. 트위터, 페이스북, 인스타그램에서 친구, 가족, 동료, 지인들과 이 기사를 공유하세요.... 감사합니다! 😁
+
+그 말과 함께 **** [**_Cup of Coffee ☕️_**](http://paypal.me/murtazasulaihi) **_와 함께 저를 응원할 수도 있습니다._**
+
+![miro.medium.com](https://miro.medium.com/v2/resize:fit:1400/1*AuT0GXEd7j21RI6uXlxYTQ.png)
+
+[https://www.twitter.com/FlutterComm](https://www.twitter.com/FlutterComm)

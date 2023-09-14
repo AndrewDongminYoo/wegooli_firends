ee877ebe8c6520cd5663411308ae0259c3a24592
sa <sa@wegooli.com>
2023-09-14 15:01:07 +0900

    :lipstick: Update the UI and style file.
    팀 초대코드 입력 다이얼로그 디자인 수정

--- lib/screens/sending_invitation_dialog.dart
+++ lib/screens/sending_invitation_dialog.dart
@@ -30,50 +30,45 @@ class _SendingInvitationDialogState extends State<SendingInvitationDialog> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-        backgroundColor: Colors.transparent,
-        contentPadding: EdgeInsets.zero,
-        insetPadding: EdgeInsets.zero,
-        content: SingleChildScrollView(
-          child: Container(
-            margin: getMargin(left: 16, right: 16, bottom: 323),
-            decoration: AppDecoration.fillOnPrimaryContainer
-                .copyWith(borderRadius: BorderRadiusStyle.circleBorder10),
-            child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Padding(
-                      padding: getPadding(left: 25, top: 15, right: 15),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                                padding: getPadding(top: 6),
-                                child: Text(
-                                  l10ns.invitationCode,
-                                  overflow: TextOverflow.ellipsis,
-                                  textAlign: TextAlign.left,
-                                  style:
-                                      CustomTextStyles.titleMedium18.copyWith(
-                                    letterSpacing: getHorizontalSize(0.04),
-                                  ),
-                                )),
-                            CustomImageView(
-                                svgPath: Assets.svg.imgCloseGray400Sharp.path,
-                                height: getSize(13),
-                                width: getSize(13),
-                                margin: getMargin(bottom: 15),
-                                onTap: goBack),
-                          ])),
-                  InvitationCodeFormField(
-                      controller: controller, readOnly: widget.copyMode),
-                  if (widget.copyMode)
-                    CopyInvitationButton(controller: userController)
-                  else
-                    ConfirmInvitationButton(controller: controller),
-                ]),
-          ),
-        ));
+      contentPadding: EdgeInsets.zero,
+      content: Container(
+        decoration: AppDecoration.fillOnPrimaryContainer
+            .copyWith(borderRadius: BorderRadiusStyle.circleBorder10),
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                  padding: getPadding(left: 25, top: 15, right: 15),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                            padding: getPadding(top: 6),
+                            child: Text(
+                              l10ns.invitationCode,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.left,
+                              style: CustomTextStyles.titleMedium18.copyWith(
+                                letterSpacing: 0.04,
+                              ),
+                            )),
+                        CustomImageView(
+                            svgPath: Assets.svg.imgCloseGray400Sharp.path,
+                            height: getSize(13),
+                            width: getSize(13),
+                            margin: getMargin(bottom: 15),
+                            onTap: goBack),
+                      ])),
+              InvitationCodeFormField(
+                  controller: controller, readOnly: widget.copyMode),
+              if (widget.copyMode)
+                CopyInvitationButton(controller: userController)
+              else
+                ConfirmInvitationButton(controller: controller),
+            ]),
+      ),
+    );
   }
 }

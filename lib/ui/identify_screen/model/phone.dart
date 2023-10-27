9169db8c36700c5f35e2543a8be9159240c202e1
Dongmin Yu <ydm2790@gmail.com>
2023-10-27 18:54:45 +0900

    ci: 💚 iOS Fastlane 설정 추가

--- lib/ui/identify_screen/model/phone.dart
+++ lib/ui/identify_screen/model/phone.dart
@@ -1,15 +1,17 @@
 // 📦 Package imports:
 import 'package:get/get_rx/get_rx.dart';
+
+// 🌎 Project imports:
 import '/data/common/dropdown_data.dart';
 
 class Telecom {
   final providers = Rx([
-      DropdownData(id: 01, title: 'KT엠모바일', value: Mobile.KT),
-      DropdownData(id: 02, title: 'LG U+', value: Mobile.LGU),
-      DropdownData(id: 03, title: 'SKT', value: Mobile.SKT),
-      DropdownData(id: 04, title: 'LG헬로모바일', value: Mobile.HELLO),
-      DropdownData(id: 05, title: '티플러스', value: Mobile.KCT),
-      DropdownData(id: 06, title: 'SK7모바일', value: Mobile.SK7),
+    DropdownData(id: 01, title: 'KT엠모바일', value: Mobile.KT),
+    DropdownData(id: 02, title: 'LG U+', value: Mobile.LGU),
+    DropdownData(id: 03, title: 'SKT', value: Mobile.SKT),
+    DropdownData(id: 04, title: 'LG헬로모바일', value: Mobile.HELLO),
+    DropdownData(id: 05, title: '티플러스', value: Mobile.KCT),
+    DropdownData(id: 06, title: 'SK7모바일', value: Mobile.SK7),
   ]);
 }
 
